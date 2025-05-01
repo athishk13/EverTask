981a2262bb1241b4c14cf25f86554ac1d8e42812
Tiger <tigeryang2005@gmail.com>
2025-05-01 15:27:06 -0700

    Basics 2

--- app/models/task.py
+++ app/models/task.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from database.db import Base
 
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id = Column(Integer, primary_key=True)
+    task_id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    description = Column(String, nullable=True)
+    due_date = Column(Date, nullable=False)
+    priority = Column(Integer, nullable=False)
+    category = Column(String, nullable=False)
--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,6 @@ from database.db import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
