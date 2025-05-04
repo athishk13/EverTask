785d8ebfb902156f03374b0fc848a04342e15598
Tiger <tigeryang2005@gmail.com>
2025-05-04 15:16:40 -0700

    About and Help menu

--- app/gui/app.py
+++ app/gui/app.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
 from database.db import init_db, SessionLocal
 from gui.login_frame import LoginFrame
 
@@ -11,6 +14,36 @@ class App(tk.Tk):
         self.db = SessionLocal()
         self.user = None
         self.switch_to_login()
+
+        # Menu bar
+        menu_bar = tk.Menu(self)
+        self.config(menu=menu_bar)
+
+        # Add Menu
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Menu", menu=help_menu)
+        help_menu.add_command(label="About", command=self.show_about)
+        help_menu.add_command(label="Help", command=self.show_help)
+
+    def show_about(self):
+        # Show about information in a pop-up
+        messagebox.showinfo("About", "EverTask is a task manager developed by Tiger Yang and Athish Kumar."
+                                     "\n ---\nIn Partial Completion for CS122 at San Jose State University.\n---"
+                                     "\nVersion 1.0")
+
+    def show_help(self):
+        # Show help information in a pop-up
+        help_text = (
+            "[Add Task]: Click add, then fill in the task form to add\n\n"
+            "[Edit Task]: Select a task by left clicking on it, click edit, then fill in the task form to make edits\n\n"
+            "[Delete Task]: Select a task by left clicking on it, then click delete\n\n"
+            "[Filter]: Select a category from the dropdown menu\n\n"
+            "[Report]: Generate a pie chart for remaining tasks by category percentage\n\n"
+            "[Sort]: Click on the heading of the category to sort tasks by its content\n\n"
+            "[Toggle Complete]: Double click on the check mark or x to toggle completeness\n\n"
+            "[Logout]: Close the window to automatically logout"
+        )
+        messagebox.showinfo("Help", help_text)
 
     # Switch active view to the login frame
     def switch_to_login(self):
